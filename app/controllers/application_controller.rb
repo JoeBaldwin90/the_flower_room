60f43c6eb4f3f40c95043a0ae1969bbd1496a1be
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-15 23:12:57 +0100

    Create a session named cart_id for new users visiting the site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+  protect_from_forgery with: :exception
+
+  before_action :current_cart
+  helper_method :current_cart
+
+  def current_cart
+    # If a new customer visits the site, give them a new cart_id.
+    if session[:cart_id].present?
+      # They have a cart, let them keep it.
+      @current_cart = Cart.find(session[:cart_id])
+    else
+      # They don't have a cart, create one for them.
+      @current_cart = Cart.create # same as Cart.new.save
+      session[:cart_id] = @current_cart.id
+    end
+  end
+
 end
