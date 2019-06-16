1d9d4314134bcbd74a7d358f34d7cfe7190e0990
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-16 15:17:36 +0100

    New order form with validations and a thankyou page.

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,22 @@
 class Order < ApplicationRecord
   has_many :order_items
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :address_1, presence: true
+  validates :city, presence: true
+  validates :country, presence: true
+
+  accepts_nested_attributes_for :order_items
+
+  def add_from_cart(cart) # pass in new variable named cart as an arguement
+
+    cart.order_items.all.each do |item|
+      self.order_items.new(product: item.product, # self is an Order record for each cart variable, which has_many order_items (e.g. Order.order_items)
+         quantity: item.quantity) # the order_items schema shows that order_items can have a product association and a quantity
+    end
+
+  end
+
 end
