2d21e04a3901a11e59429afc5819feb1bced4ca6
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-06-21 16:28:28 +0100

    Order form

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -2,14 +2,51 @@ ActiveAdmin.register Order do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-permit_params :first_name, :last_name, :email, :country, :address_1, :address_2, :city, :postal_code
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+permit_params :first_name,
+              :last_name,
+              :email,
+              :country,
+              :address_1,
+              :address_2,
+              :city,
+              :postal_code,
+              order_items_attributes: [:id, :quantity, :product_id]
+
+# Custom ActiveAdmin Product index view
+index do
+  selectable_column
+  index_column
+  column :name do |order|
+    order.first_name + " " + order.last_name
+  end
+  column :email
+  column :created_at
+  actions dropdown: false
+end
+
+form do |f|
+
+     f.inputs do
+       f.input :first_name
+       f.input :last_name
+       f.input :email
+     end
+
+     f.inputs "Address" do
+       f.input :address_1
+       f.input :address_2
+       f.input :city
+       f.input :postal_code
+       f.input :country
+     end
+
+     f.has_many :order_items, heading: "Products" do |p|
+       p.input :product
+       p.input :quantity
+     end
+
+    f.actions
+  end
+
 
 end
