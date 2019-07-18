b0581d7494a53cbb8ebc001d13fcb6abb97f2c2f
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-07-18 17:29:11 +0100

    Improved form validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,13 +1,18 @@
 class Order < ApplicationRecord
   has_many :order_items
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :phone_number, presence: true
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  phone_regex = /\A\d{11}\z/
+  postcode_regex = /\As*((GIR\s*0AA)|((([A-PR-UWYZ][0-9]{1,2})|(([A-PR-UWYZ][A-HK-Y][0-9]{1,2})|(([A-PR-UWYZ][0-9][A-HJKSTUW])|([A-PR-UWYZ][A-HK-Y][0-9][ABEHMNPRVWXY]))))\s*[0-9][ABD-HJLNP-UW-Z]{2}))\s*\z/i
+
+  validates :first_name, presence: true, length: { minimum: 2 }
+  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :email, presence: true, format: { with: email_regex }
+  validates :phone_number, presence: true, format: { with: phone_regex }
   validates :address_1, presence: true
   validates :city, presence: true
   validates :country, presence: true
+  validates :postal_code, presence: true, format: { with: postcode_regex }
 
   accepts_nested_attributes_for :order_items
 
