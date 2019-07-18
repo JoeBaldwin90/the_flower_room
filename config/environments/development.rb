3aabc61aac661f1cafa3421123a5a5f6bc797e02
Joe Baldwin <jcameronbaldwin@gmail.com>
2019-07-18 20:03:53 +0100

    development master key true

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,8 +7,7 @@ Rails.application.configure do
   config.cache_classes = false
 
   # Require master RAILS_MASTER_KEY
-  config.require_master_key = false  
-  config.read_encrypted_secrets = false
+  config.require_master_key = true  
 
   # Do not eager load code on boot.
   config.eager_load = false
